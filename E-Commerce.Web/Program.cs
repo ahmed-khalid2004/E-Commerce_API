82b7dd40a3c4f9ba4bf1559a572d3890e05198d3
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2025-05-02 22:55:34 +0300

    Part 05 Product Module - Product Service

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -2,6 +2,7 @@ using DomainLayer.Contracts;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Persistence.Data;
+using Persistence.Repositories;
 
 namespace E_Commerce.Web
 {
@@ -20,6 +21,7 @@ namespace E_Commerce.Web
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
             builder.Services.AddScoped<IDataSeeding, DataSeeding>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             #endregion
 
