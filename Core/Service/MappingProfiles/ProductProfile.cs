897c25df16799f3164278e8dfa45da8c62285471
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2025-05-02 23:05:37 +0300

    Part 06 Auto Mapper

---
+++ Core/Service/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DomainLayer.Models;
+using Shared.DataTransferObjects;
+
+namespace ApplicationLayer.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Product -> ProductDTO
+            CreateMap<Product, ProductDTO>()
+                .ForMember(dest => dest.BrandName,opt => opt.MapFrom(src => src.ProductBrand.Name))
+                .ForMember(dest => dest.TypeName,opt => opt.MapFrom(src => src.ProductType.Name));
+
+            // ProductType -> TypeDTO
+            CreateMap<ProductType, TypeDTO>();
+
+            // ProductBrand -> BrandDTO
+            CreateMap<ProductBrand, BrandDTO>();
+        }
+    }
+}
