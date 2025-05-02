7e7f56dd6463310ded8152306a035625732c3ac7
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2025-05-02 23:15:18 +0300

    Part 07 Service Manager

---
+++ Core/ServiceAbstracion/IServiceManager.cs
@@ -0,0 +1,14 @@
+﻿using DomainLayer.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesAbstraction
+{
+    public interface IServiceManager
+    {
+        IProductService ProductService { get; }
+    }
+}
---
+++ Infrastructure/Persentation/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Contracts;
+using ServicesAbstraction;
+
+namespace Presentation.Controllers
+{
+    public class ProductsController(IProductService productService)
+    {
+        private readonly IProductService _productService = productService;
+    }
+}
