5a657bdfbe4e2d8ef6daf4f756818f623cc09230
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2025-05-03 11:37:27 +0300

    Part 02 Specifications

---
+++ Core/DomainLayer/Contracts/ISpecifications.cs
@@ -0,0 +1,11 @@
+﻿using DomainLayer.Models;
+using System.Linq.Expressions;
+
+namespace DomainLayer.Contracts
+{
+    public interface ISpecification<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        Expression<Func<TEntity, bool>> Criteria { get; }
+        List<Expression<Func<TEntity, object>>> IncludeExpressions { get; }
+    }
+}
