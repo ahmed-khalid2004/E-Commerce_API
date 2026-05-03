88585eec9001e150ab1b111f669b7dbb7cfd94c2
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2026-05-03 23:34:16 +0300

    Remove Shadow FK

--- Core/DomainLayer/Exceptions/DeliveryMethodNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿
-    namespace DomainLayer.Exceptions
-    {
-    public sealed class DeliveryMethodNotFoundException(int id) : Exception($"Delivery method with id {id} was not found.")
-    {
-      
-    }
-}
