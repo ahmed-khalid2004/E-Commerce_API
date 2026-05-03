872683ee82f16dcaed746b6ae85850fadc32a9d4
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2026-05-03 23:34:16 +0300

    Remove Shadow FK

--- Core/DomainLayer/Exceptions/AddressNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace DomainLayer.Exceptions
-{
-    public sealed class AddressNotFoundException(string UserName) : NotFoundException($"Address for user {UserName} not found.")
-    {
-       
-    }
-}
