1435239696275c2c147738e67e3ed6f332cf4b1c
mahmoudgamal37 <mahmoudgamal3703@gmail.com>
2026-06-22 22:59:20 +0300

    feat(admin-customers): add customer management endpoints and service

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -36,7 +36,7 @@ namespace Services
         private readonly Lazy<ICategoryService> _lazyCategoryService = new(() => new CategoryService(unitOfWork, mapper));
         private readonly Lazy<IEmailService> _LazyEmailService = new(() => new EmailService(configuration));
         private readonly Lazy<IReviewService> _lazyReviewService = new(() => new ReviewService(unitOfWork, mapper));
-
+        private readonly Lazy<ICustomerService> _lazyCustomerService = new(() => new CustomerService(userManager, unitOfWork, mapper));
 
         public IProductService ProductService => _lazyProductService.Value;
         public IBasketService BasketService => _lazyBasketService.Value;
@@ -46,6 +46,7 @@ namespace Services
         public ICategoryService CategoryService => _lazyCategoryService.Value;
         public IEmailService EmailService => _LazyEmailService.Value;
         public IReviewService ReviewService => _lazyReviewService.Value;
+        public ICustomerService CustomerService => _lazyCustomerService.Value;
 
     }
 }
---
+++ Shared/DataTransferObjects/IdentityDTOs/CustomerStatsDTO.cs
@@ -0,0 +1,8 @@
+namespace Shared.DataTransferObjects.IdentityDTOs
+{
+    public class CustomerStatsDTO
+    {
+        public int TotalOrders { get; set; }
+        public decimal TotalSpend { get; set; }
+    }
+}
